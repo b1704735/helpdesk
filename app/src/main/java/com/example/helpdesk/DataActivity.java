a07766db16ff3dc4dd1fdc07aba4a7f010ca2127
b1704735 <53897463+b1704735@users.noreply.github.com>
2020-10-05 18:49:36 +0700

    18:49

---
+++ app/src/main/java/com/example/helpdesk/DataActivity.java
@@ -0,0 +1,53 @@
+package com.example.helpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class DataActivity extends AppCompatActivity {
+
+    ListView lvContact;
+    ArrayAdapter<String> adapter;
+    String TAG="FIREBASE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
+        lvContact=findViewById(R.id.lvContact);
+        lvContact.setAdapter(adapter);
+//lấy đối tượng FirebaseDatabase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//Kết nối tới node có tên là users (node này do ta định nghĩa trong CSDL Firebase)
+        DatabaseReference myRef = database.getReference("users");
+//truy suất và lắng nghe sự thay đổi dữ liệu
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+//vòng lặp để lấy dữ liệu khi có sự thay đổi trên Firebase
+                for (DataSnapshot data: dataSnapshot.getChildren())
+                {
+//lấy key của dữ liệu
+                    String key=data.getKey();
+//lấy giá trị của key (nội dung)
+                    String value=data.getValue().toString();
+                    adapter.add(key+"\n"+value);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/helpdesk/MainActivity.java
+++ app/src/main/java/com/example/helpdesk/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.helpdesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnDT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Thử
+        btnDT = (Button) findViewById(R.id.btnData);
+        btnDT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DataActivity.class);
+                startActivity(intent);
+                //Toast.makeText(getApplicationContext(),"dcmm",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
