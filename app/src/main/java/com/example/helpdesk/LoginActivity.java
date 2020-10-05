d58075931a9b947b79396a36f5631dd89f3e8431
Phan Hải Long <phanhailong12345678@gmail.com>
2020-10-05 17:03:03 +0700

    17:02

--- app/src/main/java/com/example/helpdesk/LoginActivity.java
+++ app/src/main/java/com/example/helpdesk/LoginActivity.java
@@ -5,34 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnDangNhap;
-    private EditText txtTaiKhoan;
-    private EditText txtMatKhau;
-    private String a="hung",b="long";
+    Button btnDangNhap;
+    EditText txtTaiKhoan, txtMatKhau;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnDangNhap = (Button) findViewById(R.id.btnDangNhap);
-        txtTaiKhoan = (EditText) findViewById(R.id.txtTaiKhoan);
-        txtMatKhau = (EditText) findViewById(R.id.txtMatKhau);
-
-        a = txtTaiKhoan.getText().toString();
-        b = txtMatKhau.getText().toString();
-
+        btnDangNhap = (Button)findViewById(R.id.btnDangNhap);
+        txtTaiKhoan = (EditText)findViewById(R.id.txtTaiKhoan);
+        txtMatKhau = (EditText)findViewById(R.id.txtMatKhau);
 
         btnDangNhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(a.equals("hung") && b.equals("long")){
+                if(txtTaiKhoan.getText().toString().equals("admin") && txtMatKhau.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(),"Dung r",Toast.LENGTH_LONG).show();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Ngu qua!",Toast.LENGTH_LONG).show();
                 }
 
             }
